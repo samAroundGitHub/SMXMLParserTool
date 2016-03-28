c8d273d086300eee263a91d6e5b8faf7cda78034
Sam <hui379190004@icloud.com>
2016-03-28 17:10:13 +0800

    Initial Commit

---
+++ SMXMLParserToolExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMXMLParserToolExample
+//
+//  Created by zzZgHhui on 16/3/28.
+//  Copyright © 2016年 zzZgHhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMXMLParserToolExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMXMLParserToolExample
+//
+//  Created by zzZgHhui on 16/3/28.
+//  Copyright © 2016年 zzZgHhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
